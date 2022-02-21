df60d452ad187d0841693485d44ad240c775dcd4
aleksandr.popov@t-systems.com <aleksandr.popov@t-systems.com>
2022-02-21 18:27:22 +0300

    add create note fragment

---
+++ app/src/main/java/com/fancysoft/androidnoteapp/fragment/AddNoteFragment.java
@@ -0,0 +1,18 @@
+package com.fancysoft.androidnoteapp.fragment;
+
+import androidx.fragment.app.Fragment;
+
+import com.fancysoft.androidnoteapp.R;
+
+/**
+ * Represents app fragment to create new note
+ */
+public class AddNoteFragment extends Fragment {
+
+    /**
+     * Passes add note fragment layout to super constructor
+     */
+    public AddNoteFragment() {
+        super(R.layout.fragment_add_note);
+    }
+}
