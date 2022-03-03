aaef47e01e3d4c4b572809c6d70a9f285df34eb0
aleksandr.popov@t-systems.com <aleksandr.popov@t-systems.com>
2022-03-03 10:52:31 +0300

    exclude AppException from test-required classes

--- app/src/main/java/com/fancysoft/androidnoteapp/exception/model/AppException.java
+++ app/src/main/java/com/fancysoft/androidnoteapp/exception/model/AppException.java
@@ -1,11 +1,13 @@
 package com.fancysoft.androidnoteapp.exception.model;
 
+import lombok.Generated;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 /**
  * Represents app exception
  */
+@Generated
 @Getter
 @RequiredArgsConstructor
 public class AppException extends RuntimeException {
